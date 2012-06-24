febeee2b2e95878fc16571f15085deb5284d3ae9
Edijs Petersons <eddpeterson@gmail.com>
2012-06-24 17:39:48 +0200

    Created rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinodeRails::Application.config.secret_token = '8621d4f9e6685a0a5bd4abb0e109a4bd31da6b05278ea36bddb7e2e7f989febbaaa22e2f3a2fe1999b5a97c727db4a405ebbf63f4dc14248047ac941d2f41cb1'
